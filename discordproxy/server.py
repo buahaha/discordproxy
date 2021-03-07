2b234f912132b3b00652ab51bf656447b594182b
ErikKalkoken <erik.kalkoken@gmail.com>
2021-03-07 19:27:57 +0100

    Ensure graceful server shutdown

--- discordproxy/server.py
+++ discordproxy/server.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import signal
 import sys
 
 import discord
@@ -14,7 +15,16 @@ from discordproxy.discord_client import DiscordClient
 logger = logging.getLogger(__name__)
 
 
+async def shutdown_server(signal, server, discord_client):
+    logger.info("Received shutdown signal: %s", signal)
+    logger.info("Logging out from Discord...")
+    await discord_client.logout()
+    logger.info("Shutting down gRPC server...")
+    await server.stop(0)
+
+
 async def run_server(token, my_args) -> None:
+    # init gRPC server and discord client
     logger.info(f"Starting {__title__} v{__version__}...")
     discord.VoiceClient.warn_nacl = False
     discord_client = DiscordClient()
@@ -22,22 +32,24 @@ async def run_server(token, my_args) -> None:
     add_DiscordApiServicer_to_server(DiscordApi(discord_client), server)
     listen_addr = f"{my_args.host}:{my_args.port}"
     server.add_insecure_port(listen_addr)
+
+    # add event handlers for graceful shutdown
+    loop = asyncio.get_event_loop()
+    signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+    for s in signals:
+        loop.add_signal_handler(
+            s,
+            lambda s=s: asyncio.create_task(shutdown_server(s, server, discord_client)),
+        )
+
+    # start the server
     msg = f"Starting gRPC server on {listen_addr}"
     logger.info(msg)
     print(msg)
     await server.start()
     asyncio.ensure_future(discord_client.start(token))
-    try:
-        await server.wait_for_termination()
-        logger.info("gRPC server has shut down")
-    except (SystemExit, KeyboardInterrupt):
-        # Shuts down the server with 0 seconds of grace period. During the
-        # grace period, the server won't accept new connections and allow
-        # existing RPCs to continue within the grace period.
-        await server.stop(0)
-    finally:
-        await discord_client.logout()
-        logger.info("Logged out from Discord")
+    await server.wait_for_termination()
+    logger.info("gRPC server has shut down")
 
 
 def main():
